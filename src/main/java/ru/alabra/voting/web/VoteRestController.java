ca8dfda119ea0c3e46e9dc2a4f8ee5abd798df83
alabra <alllexe@mail.ru>
2019-08-28 15:47:28 +0300

    init commit

--- src/main/java/ru/alabra/voting/web/VoteRestController.java
+++ src/main/java/ru/alabra/voting/web/VoteRestController.java
@@ -1,14 +1,38 @@
 package ru.alabra.voting.web;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.alabra.voting.model.Vote;
+import ru.alabra.voting.repository.VoteRepository;
+
+import java.util.List;
+
 /**
  * @author Alexander Abramov (alllexe@mail.ru)
  * @version 1
  * @since 26.08.2019
  */
-
+@RestController
+@RequestMapping(value = VoteRestController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
 public class VoteRestController {
 
     public static final String REST_URL = "/rest/vote";
+
+    @Autowired
+    protected VoteRepository repository;
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping
+    List<Vote> getAll() {
+        log.info("getAll");
+        return repository.getAll();
+    }
 
 }
---
+++ src/test/java/ru/alabra/voting/web/AdminRestControllerTest.java
@@ -0,0 +1,89 @@
+package ru.alabra.voting.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import ru.alabra.voting.model.User;
+import ru.alabra.voting.repository.UserRepository;
+
+import javax.annotation.PostConstruct;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.alabra.voting.UserTestData.*;
+
+/**
+ * @author Alexander Abramov (alllexe@mail.ru)
+ * @version 1
+ * @since 28.08.2019
+ */
+@SpringJUnitWebConfig(locations = {
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-mvc.xml",
+        "classpath:spring/spring-db.xml"
+})
+//@WebAppConfiguration
+//@ExtendWith(SpringExtension.class)
+@Transactional
+class AdminRestControllerTest {
+
+    private static final String REST_URL = AdminRestController.REST_URL + '/';
+
+    @Autowired
+    private UserRepository repository;
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private static final CharacterEncodingFilter CHARACTER_ENCODING_FILTER = new CharacterEncodingFilter();
+
+    static {
+        CHARACTER_ENCODING_FILTER.setEncoding("UTF-8");
+        CHARACTER_ENCODING_FILTER.setForceEncoding(true);
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .addFilter(CHARACTER_ENCODING_FILTER)
+//                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    void get() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(REST_URL + ADMIN_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void createWithLocation() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+}
