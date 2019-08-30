4dcef14c559068c08c77aae2203f153fff774128
alabra <alllexe@mail.ru>
2019-08-30 15:39:06 +0300

    init commit

--- src/test/java/ru/alabra/voting/web/RestarauntRestControllerTest.java
+++ src/test/java/ru/alabra/voting/web/RestarauntRestControllerTest.java
@@ -5,11 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import ru.alabra.voting.model.Restaurant;
 import ru.alabra.voting.repository.RestarauntRepository;
 import ru.alabra.voting.web.json.JsonUtil;
 
@@ -85,11 +88,41 @@ class RestarauntRestControllerTest {
 
 
     @Test
-    void delete() {
+    void delete() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete(REST_URL + BK_ID))
+                .andExpect(status().isNoContent());
+        assertMatch(repository.getAll(), MC, KFC, IL);
     }
 
     @Test
-    void update() {
+    void update() throws Exception {
+        Restaurant updated = new Restaurant(BK);
+        BK.setDescription("updated");
+
+        mockMvc.perform(MockMvcRequestBuilders.put(REST_URL + updated.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonUtil.writeValue(updated)))
+                .andExpect(status().isNoContent());
+
+        assertMatch(repository.get(BK_ID), updated);
+    }
+
+    @Test
+    void save() throws Exception {
+        Restaurant created = new Restaurant(null, "King Shrimps", "super duper");
+
+        ResultActions action = mockMvc.perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonUtil.writeValue(created)));
+
+        MvcResult result = action.andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+
+        Restaurant returned = jsonUtil.readValue(contentAsString, Restaurant.class);
+        created.setId(returned.getId());
+
+        assertMatch(action, created);
+        assertMatch(repository.getAll(), MC, KFC, BK, IL, created);
     }
 
 }
