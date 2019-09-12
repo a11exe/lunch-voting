c9a4b12eaed3b83a29b0f1ebb144dacf5419698d
alabra <alllexe@mail.ru>
2019-09-12 10:31:17 +0300

    replace postgres on h2

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
-  id               SERIAL PRIMARY KEY,
+  id               INTEGER AUTO_INCREMENT PRIMARY KEY,
   name             VARCHAR                 NOT NULL,
   email            VARCHAR                 NOT NULL,
   password         VARCHAR                 NOT NULL,
@@ -19,21 +19,21 @@ CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_roles
 (
-  user_id SERIAL PRIMARY KEY,
+  user_id INTEGER AUTO_INCREMENT PRIMARY KEY,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
 CREATE TABLE restaurant (
-  id          SERIAL PRIMARY KEY,
+  id          INTEGER AUTO_INCREMENT PRIMARY KEY,
   name        VARCHAR   NOT NULL,
   description TEXT      NOT NULL,
   CONSTRAINT name_idx UNIQUE (name)
 );
 
 CREATE TABLE menu (
-  id                    SERIAL PRIMARY KEY,
+  id                    INTEGER AUTO_INCREMENT PRIMARY KEY,
   date                  DATE DEFAULT now() NOT NULL,
   description           TEXT      NOT NULL,
   restaurant_id         INTEGER NOT NULL,
@@ -42,11 +42,11 @@ CREATE TABLE menu (
 );
 
 CREATE TABLE vote (
-  id                    SERIAL PRIMARY KEY,
+  id                    INTEGER AUTO_INCREMENT PRIMARY KEY,
   date                  DATE DEFAULT now() NOT NULL,
   menu_id               INTEGER NOT NULL,
   user_id               INTEGER NOT NULL,
   CONSTRAINT user_date_idx UNIQUE (user_id, date),
   FOREIGN KEY (menu_id) REFERENCES menu (id) ON DELETE CASCADE,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-)
+);
