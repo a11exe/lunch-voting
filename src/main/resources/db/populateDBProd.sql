a53d273edca62245dd2c357ee7c449d4896d655a
alabra <alllexe@mail.ru>
2019-09-17 16:04:29 +0300

    refactoring add users prod

--- src/main/resources/db/populateDBProd.sql
+++ src/main/resources/db/populateDBProd.sql
@@ -1,1 +1,7 @@
-SELECT 1
+INSERT INTO users (id, name, email, password) VALUES
+  (100000, 'Admin', 'admin@gmail.com', '{noop}admin'),
+  (100001, 'Alex', 'alex@yandex.ru', '{noop}password');
+
+INSERT INTO user_roles (role, user_id) VALUES
+  ('ROLE_ADMIN', 100000),
+  ('ROLE_USER', 100001);
